e6802d4262263273e031fb8cce1741eeb71b72e3
Ottomann <ott.ottender@gmail.com>
2023-11-19 14:30:15 +0200

    first commit

---
+++ OROperator/Program.cs
@@ -0,0 +1,25 @@
+﻿// math >= 90 or chem >= 90 Or bio >= 90
+
+//math && chem || math && biology || chem && bio
+ 
+int math, chemistry, biology;
+
+
+Console.WriteLine("Enter your Math result:");
+math = Int32.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Enter your Chemisty result:");
+chemistry  = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Biology result:");
+biology = Int32.Parse(Console.ReadLine());
+     // kas sulgude sees olev block on true //block() saab tingimuseks
+if ((math >= 90 && chemistry >= 90) || (math >= 90 && biology >= 90) || (chemistry >= 90 && biology >=90)) //kui uks neist tingimustest on true ss accepted
+{
+    Console.WriteLine("Congratulations! You got accepted.");
+}
+else 
+{
+ Console.WriteLine("Your application cannot be approved. ");   
+}
---
+++ Week5LogicalOperators/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+//and - && //nt mata eksami tulemus sisseastumisel /RANGE
+
+//mata //f22sika //keemia
+//true && true && true--> true //kirjeldab tingimusi, mis kui t2itmisel v2ljastavad v22rtuse true
+//true && false --> false
+//false && true --> false
+//false && false --> false
+
+//OR - || (pipes) /Paindlikum
+
+//true || true --> true // uks tingimus peab olema t2idetud
+//true || false --> true
+//false || true --> true
+//false || false --> false
